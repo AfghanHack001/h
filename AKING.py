a9bedd88c2d7976c67b826841c173647454a6961
AFGHAN HACK <110977192+AfghanHack001@users.noreply.github.com>
2022-12-09 07:47:19 +0430

    Create AKING.py

---
+++ AKING.py
@@ -0,0 +1,8 @@
+import os
+from platform import uname
+arch=uname().machine.lower()
+if 'aarch' in arch:
+    arch = 'aarch'
+    print('\033[1;32m\nCongratulatings! Your Deviec Support This Tools')
+    import AKINGTATA
+else:exit('\033[1;31m Sorry System or device not supported ')
